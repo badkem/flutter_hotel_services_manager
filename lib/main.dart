bdb7288504589459991571e282dda9235c562138
Rio Cheng <hoangdinhthangloi@gmail.com>
2021-03-26 00:13:11 +0700

    Setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-void main() {
-  runApp(Home());
+void main() => runApp(GetMaterialApp(home: Home()));
+
+class Controller extends GetxController {
+  var count = 0.obs;
+
+  increment() => count++;
+
+  resetCount() => count.value = 0;
 }
 
-class Home extends StatefulWidget {
+class Home extends StatelessWidget {
   @override
-  _HomeState createState() => _HomeState();
-}
+  Widget build(context) {
+    // Instantiate your class using Get.put() to make it available for all "child" routes there.
+    final Controller c = Get.put(Controller());
 
-class _HomeState extends State<Home> {
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Demo"),
-        elevation: 0.5,
-      ),
-      body: Center(
-        child: Text("Khóa luận tốt nghiệp"),
-      ),
-    );
+        // Use Obx(()=> to update Text() whenever count is changed.
+        appBar: AppBar(title: Obx(() => Text("Clicks: ${c.count}"))),
+
+        // Replace the 8 lines Navigator.push by a simple Get.to(). You don't need context
+        body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(
+                child: Text("Go to Other"), onPressed: () => Get.to(Other())),
+            ElevatedButton(child: Text("Reset"), onPressed: c.resetCount),
+          ],
+        )),
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add), onPressed: c.increment));
+  }
+}
+
+class Other extends StatelessWidget {
+  // You can ask Get to find a Controller that is being used by another page and redirect you to it.
+  final Controller c = Get.find();
+
+  @override
+  Widget build(context) {
+    // Access the updated count variable
+    return Scaffold(body: Center(child: Text("${c.count}")));
   }
 }
