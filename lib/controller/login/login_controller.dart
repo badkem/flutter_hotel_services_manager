fc5e2c16f2055ac8dade965e9d8ac0eddd1a0724
Rio Cheng <hoangdinhthangloi@gmail.com>
2021-04-10 00:57:36 +0700

    Check room exist on login

--- lib/controller/login/login_controller.dart
+++ lib/controller/login/login_controller.dart
@@ -1,7 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:khoaluantotnghiep2021/data/repository/repository.dart';
+import 'package:khoaluantotnghiep2021/ui/home/home_page.dart';
+import 'package:khoaluantotnghiep2021/ui/theme/app_colors.dart';
+import 'package:meta/meta.dart';
 
 class LoginController extends GetxController {
-  TextEditingController textEditingController;
+  TextEditingController textRoomName, textLabel;
+  String roomName, label = '';
 
+  checkRoomExist() {
+    if (textRoomName.text.isNotEmpty && textLabel.text.isNotEmpty) {
+      MyRepository().checkRoomExistRequest(roomName, label);
+      Get.to(HomePage());
+    } else {
+    showDialog();
+    }
+  }
+
+  showDialog(){
+    return Get.defaultDialog(
+        title: "Login Failed",
+        titleStyle: TextStyle(
+          color: AppColors.primaryTextColor,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+        content: Column(
+          children: [
+            Text("Please contact via manager!"),
+            SizedBox(height: 10),
+            TextButton(
+              child: Text('Okay'),
+              style: TextButton.styleFrom(
+                textStyle: TextStyle(
+                  fontSize: 12,
+                ),
+                primary: Colors.white,
+                backgroundColor: AppColors.primaryColor,
+              ),
+              onPressed: () => Get.back(),
+            ),
+          ],
+        ));
+  }
+  @override
+  void onInit() {
+    textRoomName = new TextEditingController();
+    textLabel = new TextEditingController();
+    super.onInit();
+  }
+
+  @override
+  void onClose() {
+    textRoomName.dispose();
+    textLabel.dispose();
+    super.onClose();
+  }
 }
--- lib/ui/login/login_page.dart
+++ lib/ui/login/login_page.dart
@@ -11,78 +11,85 @@ class LoginPage extends GetView<LoginController> {
     height = MediaQuery.of(context).size.height;
     width = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Center(
-        child: Container(
-          height: height * 0.55,
-          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-          margin: EdgeInsets.symmetric(horizontal: 24),
-          decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(8),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.blueGrey.withOpacity(0.2),
-                  spreadRadius: 6,
-                  blurRadius: 7,
-                  offset: Offset(0, 2),
-                ),
-              ]),
-          child: Column(
-            children: [
-              Image.asset(
-                'assets/images/ic_logo_app_foreground.png',
-                height: 140,),
-              SizedBox(height: height * 0.05),
-              TextField(
-                autofocus: false,
-                style:
-                    TextStyle(fontSize: 20, color: AppColors.primaryTextColor),
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  hintText: 'Room Name',
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(width: 1.5, color: Colors.black54),
-                  ),
-                ),
-              ),
-              SizedBox(height: height * 0.04),
-              TextField(
-                autofocus: false,
-                style: TextStyle(
-                    fontSize: 20, color: AppColors.primaryTextColor),
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  hintText: 'A',
-                  hintStyle: TextStyle(color: Colors.black87),
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                        width: 1.5,
-                        color: Colors.black54),
-                  ),
-                ),
-              ),
-              SizedBox(height: height * 0.03),
-              Container(
-                width: width * 1.0,
-                child: TextButton(
-                  child: Text('Sign In'),
-                  style: TextButton.styleFrom(
-                    textStyle: TextStyle(
-                      fontSize: 20,
+      body: GetBuilder(
+        init: controller,
+        builder: (_) {
+          return Center(
+            child: Container(
+              height: height * 0.55,
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              margin: EdgeInsets.symmetric(horizontal: 24),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.blueGrey.withOpacity(0.2),
+                      spreadRadius: 6,
+                      blurRadius: 7,
+                      offset: Offset(0, 2),
                     ),
-                    primary: Colors.white,
-                    backgroundColor: AppColors.primaryColor,
+                  ]),
+              child: Column(
+                children: [
+                  Image.asset(
+                    'assets/images/ic_logo_app_foreground.png',
+                    height: 140,),
+                  SizedBox(height: height * 0.05),
+                  TextField(
+                    controller: controller.textRoomName,
+                    onChanged: (value) => controller.roomName = value,
+                    autofocus: false,
+                    style:
+                    TextStyle(fontSize: 20, color: AppColors.primaryTextColor),
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      hintText: 'Room Name',
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(width: 1.5, color: Colors.black54),
+                      ),
+                    ),
                   ),
-                  onPressed: () {
-                    print('Pressed');
-                  },
-                ),
-              )
-            ],
-          ),
-        ),
+                  SizedBox(height: height * 0.04),
+                  TextField(
+                    controller: controller.textLabel,
+                    onChanged: (value) => controller.label = value,
+                    autofocus: false,
+                    style: TextStyle(
+                        fontSize: 20, color: AppColors.primaryTextColor),
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      hintText: 'A',
+                      hintStyle: TextStyle(color: Colors.black87),
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                            width: 1.5,
+                            color: Colors.black54),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: height * 0.03),
+                  Container(
+                    width: width * 1.0,
+                    child: TextButton(
+                      child: Text('Sign In'),
+                      style: TextButton.styleFrom(
+                        textStyle: TextStyle(
+                          fontSize: 20,
+                        ),
+                        primary: Colors.white,
+                        backgroundColor: AppColors.primaryColor,
+                      ),
+                      onPressed: () => controller.checkRoomExist(),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
